fcf38cbdf82b0e2f5d280bccd94aa821e61216d5
Simon Huang <simon@simonhuang.ca>
2019-01-19 23:04:51 -0500

    Endpoint changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
     res.send('Server is running!');
 });
 
-app.put('/updateRating', (request, response) => {
+app.post('/updateRating', (request, response) => {
     async function updateRating() {
         let bodyPlaceId = request.body.placeId;
         let bodyUserId = request.body.userId;
@@ -27,33 +27,6 @@ app.put('/updateRating', (request, response) => {
 
         await dbAPI.addUserAlreadyRatedPlace(bodyUserId, bodyPlaceId);
         response.send(newPlaceObj);
-    }
-    updateRating();
-});
-
-app.post('/updateRating', (request, response) => {
-    /*
-        1. takes place name and returns a place object
-        2. gets the user rating which is between 1-5
-        3. place object contains the current rating and the number of people that have rated it
-        4. process data to create new rating for the place and
-        5. update the rating, and the places that the person has visited
-    */
-    async function updateRating() {
-        let placeObject = await dbAPI.getPlaceData(request.body.placeName);
-        let userRating = request.body.userRating;
-        let userObject = await dbAPI.getUserData(request.body.userID);
-
-        var currentRating = placeObject.placeRating;
-        var totalRatings = placeObject.placeRating;
-
-        var newRating = (currentRating + userRating) / (totalRatings + 1);
-
-
-        await dbAPI.updateRating(placeObject.placeName, newRating);
-
-        await dbAPI.addUserAlreadyRatedPlace(userObject._id, placeObject);
-        response.send('Updated!');
     }
     updateRating();
 });
